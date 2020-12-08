b42bd0e0020b6c2db540f590d85c3aad6e18b162
amit <amit.an@robosoftin.com>
2020-12-08 20:22:39 +0530

    students details first commit after prettier

--- src/components/searchField/search.js
+++ src/components/searchField/search.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./search.css";
 
-export default function Search({setValue}) {
-    return (
-        <div className='search'>
-            <input className='searchInput' type='search' placeholder='Search by name' onChange={(e) => setValue(e.target.value)}></input>
-        </div>
-    )
+export default function Search({ setValue }) {
+  return (
+    <div className="search">
+      <input
+        className="searchInput"
+        type="search"
+        placeholder="Search by name"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+    </div>
+  );
 }
